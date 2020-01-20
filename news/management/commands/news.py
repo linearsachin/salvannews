832395b24bfdff1125f897fe59a5c358872e5b7e
SacHin Yadav <sachineg39@gmail.com>
2020-01-20 23:14:59 +0530

    aaaaaddd

--- news/management/commands/news.py
+++ news/management/commands/news.py
@@ -1,11 +1,10 @@
 import requests 
 from bs4 import BeautifulSoup 
 import csv 
-import bitly_api
 from django.core.management.base import BaseCommand
 from news.models import Category, NewsData
 from salvannews.settings import MY_ACCESS_TOKEN
-
+from bitly_api import bitly_api
 
 
 class Command(BaseCommand):
