7f9b823c284f13638f361d52c244c5248ac50212
SacHin Yadav <sachineg39@gmail.com>
2020-02-27 20:09:31 +0530

    aa

--- news/views.py
+++ news/views.py
@@ -8,7 +8,7 @@ import datetime
 class Homeview(ListView):
     def get(self, request, *args, **kwargs): 
         news1 = NewsData.objects.all().order_by('-time')  
-        news =  delete_news_after_30(news1)
+        news =  delete_news_after_60(news1)
         context={
             'NewsData':news,
             'category':NEWS_CATEGORY,
@@ -53,9 +53,9 @@ def category_news(request,category,*args,**kwargs):
    
         return render(request,'news/category_news.html',context)
 
-def delete_news_after_30(news):
+def delete_news_after_60(news):
     for news_ in news:
-        if news_.get_time_diff() > 30:
+        if news_.get_time_diff() > 60:
             news_.delete()
         
     return news
